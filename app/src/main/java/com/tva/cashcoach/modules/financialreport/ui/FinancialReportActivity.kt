8b04e13035bd743f105dbe0c0e9de4d74df35b5e
MesaricJakob <46690571+MesaricJakob@users.noreply.github.com>
2023-06-02 01:25:46 +0200

    Added graoph and prepared everything for data in graph

--- app/src/main/java/com/tva/cashcoach/modules/financialreport/ui/FinancialReportActivity.kt
+++ app/src/main/java/com/tva/cashcoach/modules/financialreport/ui/FinancialReportActivity.kt
@@ -5,19 +5,38 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartView
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 import com.tva.cashcoach.R
 import com.tva.cashcoach.appcomponents.base.BaseActivity
+import com.tva.cashcoach.appcomponents.model.transaction.Transaction
+import com.tva.cashcoach.appcomponents.model.transaction.TransactionDao
+import com.tva.cashcoach.appcomponents.persistence.repository.transaction.TransactionRepository
 import com.tva.cashcoach.databinding.ActivityFinancialReportBinding
 import com.tva.cashcoach.modules.financialreport.data.model.Listtrash1RowModel
 import com.tva.cashcoach.modules.financialreport.data.model.SpinnerDropdownMonthModel
 import com.tva.cashcoach.modules.financialreport.data.model.SpinnerDropdownTransaModel
 import com.tva.cashcoach.modules.financialreport.data.viewmodel.FinancialReportVM
+import com.tva.cashcoach.modules.transaction.data.model.TransactionRowModel
+import com.tva.cashcoach.modules.transaction.ui.TransactionAdapter
+import kotlinx.coroutines.launch
 
 class FinancialReportActivity :
     BaseActivity<ActivityFinancialReportBinding>(R.layout.activity_financial_report) {
     private val viewModel: FinancialReportVM by viewModels()
 
     private val REQUEST_CODE_FINANCIAL_REPORT_PIE_CHART_ACTIVITY: Int = 545
+
+    private lateinit var transactionAdapter: TransactionAdapter
+
+    private lateinit var transactionRepository: TransactionRepository
+
+    private lateinit var transactionDao: TransactionDao
+
+    private lateinit var transactions: List<Transaction>
 
     override fun onInitialized() {
         viewModel.navArguments = intent.extras?.getBundle("bundle")
@@ -28,6 +47,10 @@ class FinancialReportActivity :
             SpinnerDropdownMonthModel("Item4"),
             SpinnerDropdownMonthModel("Item5")
         )
+
+        transactionDao = appDb.getTransactionDao()
+        transactionRepository = TransactionRepository(transactionDao)
+
 //        val spinnerDropdownMonthAdapter =
 //            SpinnerDropdownMonthAdapter(
 //                this,
@@ -35,6 +58,12 @@ class FinancialReportActivity :
 //                viewModel.spinnerDropdownMonthList.value ?: mutableListOf()
 //            )
 //        binding.spinnerDropdownMonth.adapter = spinnerDropdownMonthAdapter
+
+        transactionAdapter = TransactionAdapter(
+            transactionRepository,
+            preferenceHelper
+        )
+
         viewModel.spinnerDropdownTransaList.value = mutableListOf(
             SpinnerDropdownTransaModel("Item1"),
             SpinnerDropdownTransaModel("Item2"),
@@ -49,19 +78,58 @@ class FinancialReportActivity :
 //                viewModel.spinnerDropdownTransaList.value ?: mutableListOf()
 //            )
 //        binding.spinnerDropdownTransa.adapter = spinnerDropdownTransaAdapter
-        val listtrashAdapter = ListtrashAdapter(viewModel.listtrashList.value ?: mutableListOf())
-        binding.recyclerListtrash.adapter = listtrashAdapter
-        listtrashAdapter.setOnItemClickListener(
-            object : ListtrashAdapter.OnItemClickListener {
-                override fun onItemClick(view: View, position: Int, item: Listtrash1RowModel) {
-                    onClickRecyclerListtrash(view, position, item)
+
+        binding.recyclerTransactions.adapter = transactionAdapter
+
+        transactionAdapter.setOnItemClickListener(
+            object : TransactionAdapter.OnItemClickListener {
+                override fun onItemClick(
+                    view: View,
+                    position: Int,
+                    transaction: TransactionRowModel
+                ) {
+                    onClickRecyclerTransactions(view, position, transaction)
                 }
             }
         )
-        viewModel.listtrashList.observe(this) {
-            listtrashAdapter.updateData(it)
-        }
         binding.financialReportLineChartVM = viewModel
+
+        lifecycleScope.launch {
+            transactions = transactionAdapter.fetchAllTransactions(preferenceHelper.getString("curr_wallet_id", ""))
+            transactionAdapter.fetchTransactions(preferenceHelper.getString("curr_wallet_id", ""))
+
+            if (preferenceHelper.getString("curr_user_currency", "") == "EUR") {
+                binding.valBudget.text = String.format(
+                    "%.2f€",
+                    transactionAdapter.fetchIncomesSum(preferenceHelper.getString("curr_wallet_id", "")) - transactionAdapter.fetchExpensesSum(
+                        preferenceHelper.getString("curr_wallet_id", "")
+                    )
+                )
+            } else {
+                binding.valBudget.text = String.format(
+                    "%.2f$",
+                    transactionAdapter.fetchIncomesSum(preferenceHelper.getString("curr_wallet_id", "")) - transactionAdapter.fetchExpensesSum(
+                        preferenceHelper.getString("curr_wallet_id", "")
+                    )
+                )
+            }
+        }
+
+        val chart = findViewById<AAChartView>(R.id.chartView)
+        val aaChartModel : AAChartModel = AAChartModel()
+            .chartType(AAChartType.Area)
+            .dataLabelsEnabled(true)
+            .colorsTheme(arrayOf("#3D85C6"))
+            .series(
+                arrayOf(
+                    AASeriesElement()
+                    .name("Budget")
+                        //namesto fiksnih podatkov uporabi podatke iz transactions (idi cez list, in shrani .value v array)
+                    .data(arrayOf(1000.0, 800.0, 850.0, 700.0, 500.0, 100.0, 300.0))
+                )
+            )
+        chart.aa_drawChartWithChartModel(aaChartModel)
+
     }
 
     override fun setUpClicks() {
@@ -70,10 +138,10 @@ class FinancialReportActivity :
         }
     }
 
-    fun onClickRecyclerListtrash(
+    fun onClickRecyclerTransactions(
         view: View,
         position: Int,
-        item: Listtrash1RowModel
+        item: TransactionRowModel
     ): Unit {
         when (view.id) {
         }
--- app/src/main/java/com/tva/cashcoach/modules/transaction/ui/TransactionAdapter.kt
+++ app/src/main/java/com/tva/cashcoach/modules/transaction/ui/TransactionAdapter.kt
@@ -120,6 +120,13 @@ class TransactionAdapter(
         notifyDataSetChanged()
     }
 
+    suspend fun fetchAllTransactions(wallet_id: String): List<Transaction> {
+        return withContext(Dispatchers.IO) {
+            transactionRepository.getAll(wallet_id)
+        }
+        notifyDataSetChanged()
+    }
+
     suspend fun fetchIncomesSum(wallet_id: String): Double {
         return withContext(Dispatchers.IO) {
             transactionRepository.getIncomesSum(wallet_id)
