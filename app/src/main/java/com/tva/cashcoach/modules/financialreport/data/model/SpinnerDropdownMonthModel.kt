61bad6a254a20e2c1769be064222e291a4210f83
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-06-03 15:47:20 +0200

    Remove unnecessary files

--- app/src/main/java/com/tva/cashcoach/modules/financialreport/data/model/SpinnerDropdownMonthModel.kt
+++
@@ -1,5 +0,0 @@
-package com.tva.cashcoach.modules.financialreport.`data`.model
-
-data class SpinnerDropdownMonthModel(
-    val itemName: String
-)
--- app/src/main/java/com/tva/cashcoach/modules/financialreport/data/viewmodel/FinancialReportVM.kt
+++ app/src/main/java/com/tva/cashcoach/modules/financialreport/data/viewmodel/FinancialReportVM.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.tva.cashcoach.modules.financialreport.data.model.FinancialReportModel
-import com.tva.cashcoach.modules.financialreport.data.model.Listtrash1RowModel
-import com.tva.cashcoach.modules.financialreport.data.model.SpinnerDropdownMonthModel
-import com.tva.cashcoach.modules.financialreport.data.model.SpinnerDropdownTransaModel
 import org.koin.core.KoinComponent
 
 class FinancialReportVM : ViewModel(), KoinComponent {
@@ -14,13 +11,4 @@ class FinancialReportVM : ViewModel(), KoinComponent {
         MutableLiveData(FinancialReportModel())
 
     var navArguments: Bundle? = null
-
-    val spinnerDropdownMonthList: MutableLiveData<MutableList<SpinnerDropdownMonthModel>> =
-        MutableLiveData()
-
-    val spinnerDropdownTransaList: MutableLiveData<MutableList<SpinnerDropdownTransaModel>> =
-        MutableLiveData()
-
-    val listtrashList: MutableLiveData<MutableList<Listtrash1RowModel>> =
-        MutableLiveData(mutableListOf())
 }
