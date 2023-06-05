44971145f104ce2f581aaf0cbbc0021b7ee4e576
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-06-05 20:28:03 +0200

    Add comments

--- app/src/main/java/com/tva/cashcoach/infrastructure/utility/Converters.kt
+++ app/src/main/java/com/tva/cashcoach/infrastructure/utility/Converters.kt
@@ -3,12 +3,26 @@ package com.tva.cashcoach.infrastructure.utility
 import androidx.room.TypeConverter
 import java.util.Date
 
+/**
+ * Converters for Room database
+ */
 class Converters {
+
+    /**
+     * Convert from timestamp to date
+     * @param value
+     * @return Date
+     */
     @TypeConverter
     fun fromTimestamp(value: Long?): Date? {
         return value?.let { Date(it) }
     }
 
+    /**
+     * Convert from date to timestamp
+     * @param date
+     * @return Long
+     */
     @TypeConverter
     fun dateToTimestamp(date: Date?): Long? {
         return date?.time?.toLong()
